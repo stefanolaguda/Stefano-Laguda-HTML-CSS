ee2c0fc5b48f45a91728ec0aa5263359eb3a966a
EarthDragon10 <lagudadev@gmail.com>
2022-01-01 23:30:23 +0100

    final deploy of site

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,22 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBVx1p5n5sZILxA4nwA5KkfBDMqJikGrTY",
-  authDomain: "portfolio-940f1.firebaseapp.com",
-  projectId: "portfolio-940f1",
-  storageBucket: "portfolio-940f1.appspot.com",
-  messagingSenderId: "486879235305",
-  appId: "1:486879235305:web:1ee35d0c1e911d0d6b3478",
-  measurementId: "G-XMJ0FRD0K3"
+  apiKey: "AIzaSyB93aeONv0Z2yXp1bOuM5dWJ7zrsBlMs_Y",
+  authDomain: "portfolio-stefano-laguda-49c6f.firebaseapp.com",
+  projectId: "portfolio-stefano-laguda-49c6f",
+  storageBucket: "portfolio-stefano-laguda-49c6f.appspot.com",
+  messagingSenderId: "56195200940",
+  appId: "1:56195200940:web:7bf107b0bd31d18da1ec09",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
